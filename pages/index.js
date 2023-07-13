7a42b47d72f9634dc38756d9e36ee8addc1546cb
Dev-Yong <dragonyongs@naver.com>
2023-07-13 19:01:12 +0900

    Sidevar and TopCards working...

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-import Head from 'next/head'
-import Header from '../components/Header'
+import Head from 'next/head';
+import Image from 'next/image';
+
+import Header from '../components/Header';
+import TopCards from '../components/TopCards';
 
 export default function Home() {
   return (
@@ -10,8 +13,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gray-100">
+      <main className="bg-gray-100 min-h-screen">
         <Header />
+        <TopCards />
       </main>
     </>
   )
